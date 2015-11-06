f780c442f47e2e50d1a4d02fbad7fc73f8764c34
David Ross Carley <mouseboyx@gmail.com>
2015-11-06 16:16:42 +0000

    the index has been changed to an htm file and the old index is now oldindex.php

--- oldindex.php
+++ oldindex.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 $filesArray = scandir('.');
 foreach ($filesArray as $file) {
     echo '<a href="'.$file.'">'.$file.'</a><br>';
 }
 ?>
+</body>
+</html>
+
+
+
+
